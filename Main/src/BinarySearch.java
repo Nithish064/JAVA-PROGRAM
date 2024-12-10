3892f6d1a4d5041997040cc0fdb9b3d5dd39b3a7
Nithish064 <nithishs829@gmail.com>
2024-12-10 19:57:56 +0530

    Initial commit

--- Main/src/BinarySearch.java
+++ Main/src/BinarySearch.java
@@ -12,18 +12,35 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
 
+        boolean isAsc = arr[start] < arr[end];
+
         while(start <= end) {
             // find the middle element
 //            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
+            if(arr[mid] == target){
                 return mid;
+            }
+
+            if(isAsc)   
+            {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else{
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                if (target > arr[mid]) 
+                {
+                    end = mid - 1;
+                } 
+                else
+                {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
---
+++ Main/src/greatestamongthreenumbers.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class greatestamongthreenumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input three numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        // Find the greatest using if-else statements
+        int greatest;
+
+        if (num1 > num2) {
+            if (num1 > num3) {
+                greatest = num1;
+            } else {
+                greatest = num3;
+            }
+        } else {
+            if (num2 > num3) {
+                greatest = num2;
+            } else {
+                greatest = num3;
+            }
+        }
+
+        // Output the result
+        System.out.println("The greatest number is: " + greatest);
+    }
+
+}
