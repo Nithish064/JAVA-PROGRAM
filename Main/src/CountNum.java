0f5f18f0c535a3d0d86f94af82239565aff217ce
Nithish064 <nithishs829@gmail.com>
2024-12-12 19:28:11 +0530

    modified

---
+++ Main/src/CountNum.java
@@ -0,0 +1,16 @@
+public class CountNum {
+    public static void main(String[] args) {
+        int n = 233533;
+        int c = 0;
+        while (n > 0)
+        {
+            int r = n % 10;
+            if(r == 3)
+            {
+                c++;
+            }
+            n /= 10;
+        }
+        System.out.println(c);
+    }
+}
---
+++ Main/src/SwapArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SwapArray 
+{
+    public static void main(String[] args) {
+        int[] a = {1,2,4,5,6};
+        Swap(a,1,4);
+
+        System.out.println(Arrays.toString(a));
+    }
+
+    static void Swap(int[] arr, int index1, int index2)
+    {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
