1391df053375e7dc9e8dfa252d928fbad6538d79
Nithish064 <nithishs829@gmail.com>
2024-12-20 19:31:47 +0530

    modified

---
+++ Main/src/Strings/Strings/SumOfStringInNum.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import java.util.regex.*;
+
+public class SumOfStringInNum {
+    public static void main(String[] args) {
+        String str = "1 cat and 2 dogs";
+
+        int sum = 0;
+
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(str);
+
+        while(matcher.find())
+        {
+            sum += Integer.parseInt(matcher.group());
+        }
+        System.out.println("Sum : " + sum);
+
+    }
+}
